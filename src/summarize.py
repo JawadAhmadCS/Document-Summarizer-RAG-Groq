002180294a3d3798c0e8077341f89a70dadf9564
M Jawad Ahmad <126815257+JawadAhmadCS@users.noreply.github.com>
2025-06-15 05:37:15 +0500

    Update summarize.py

--- src/summarize.py
+++ src/summarize.py
@@ -1,17 +1,13 @@
+import streamlit as st
 from langchain.chains import RetrievalQA
-from langchain.chains.question_answering import load_qa_chain
-from langchain.chains.llm import LLMChain
-from langchain_core.prompts import PromptTemplate
 from langchain_groq import ChatGroq
 
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-def make_summarizer(vector_store):
+def make_summarizer(vector_store, model: str, temp: float, max_tokens: int):
     llm = ChatGroq(
-        model=os.getenv("GROQ_CHAT_MODEL", "llama3-70b-8192"), 
-        api_key=os.getenv("GROQ_API_KEY")  # fetch actual key
+        model=model,
+        temperature=temp,
+        max_tokens=max_tokens,
+        api_key=st.secrets["GROQ_API_KEY"]
     )
 
     chain = RetrievalQA.from_chain_type(
