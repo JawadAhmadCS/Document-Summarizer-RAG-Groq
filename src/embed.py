855c3e8605e5c11acad418b6930f5cedde025c19
M Jawad Ahmad <126815257+JawadAhmadCS@users.noreply.github.com>
2025-06-15 05:37:39 +0500

    Update embed.py

--- src/embed.py
+++ src/embed.py
@@ -2,8 +2,7 @@ from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_core.documents import Document
 
-def embed_uploaded_text(chunks: list[str]):
-    emb = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+def build_or_load_index(chunks: list[str]):
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     docs = [Document(page_content=chunk) for chunk in chunks]
-    vs = FAISS.from_documents(docs, embedding=emb)
-    return vs
+    return FAISS.from_documents(docs, embedding=embeddings)
